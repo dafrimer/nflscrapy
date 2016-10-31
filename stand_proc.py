8360039b7fe1e9baa4cda2fd24784fb47781829f
Daniel Frimer <danielfrimer@gmail.com>
2016-10-30 23:58:03 -0700

    added error class for page not found

--- stand_proc.py
+++ stand_proc.py
@@ -3,3 +3,8 @@ def simpconvert_to_str(val):
         return str(val)
     except UnicodeEncodeError:
         return e.text.encode('ascii', 'ignore')
+
+class NFL_Schedule_Error(Exception):
+    def __init__(self, message):
+        self.message = message
+
